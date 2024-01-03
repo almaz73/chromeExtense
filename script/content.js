183a7dd484cc81162e90d08ac5cb039cf23fc938
almaz <almaz_73@mail.ru>
2024-01-03 11:18:38 +0300

    with Vuу+ElementPlus

--- script/content.js
+++ script/content.js
@@ -1,44 +1,23 @@
 chrome.runtime.onMessage.addListener( // this is the message listener
-    function(request, sender, sendResponse) {
-        console.log('>>>'+request);
+    function (request, sender, sendResponse) {
+        console.log('>>>' + request);
         getData(sendResponse)
     }
 );
 
 
-function getData(sendResponse){
+function getData(sendResponse) {
     let fields = document.querySelectorAll('[data-marker="item"]')
-// let article = fields[0]
-    const article = document.querySelector('[class="content"]');
 
-    console.log('fields', fields)
+    let data = []
+    fields.forEach(el => {
+        let id = el.id
+        let title = el.querySelector(".iva-item-titleStep-pdebR > div > a").title
+        let content = el.querySelector('meta').content
+        data.push({id, title, content})
+    })
 
-    // if (article) {
-    //     const text = article.textContent;
-    //     // console.log('text', text)
-    //
-    //     const wordMatchRegExp = /[^\s]+/g; // Regular expression
-    //     const words = text.matchAll(wordMatchRegExp);
-    //
-    //     // console.log('words', words)
-    //     // matchAll returns an iterator, convert to array to get word count
-    //     const wordCount = [...words].length;
-    //     const readingTime = Math.round(wordCount / 200);
-    //     const badge = document.createElement("p");
-    //     // Use the same styling as the publish information in an article's header
-    //     badge.classList.add("color-secondary-text", "type--caption");
-    //     badge.textContent = `1111111111111111111111111111111 ⏱️ ${readingTime} min read`;
-    //
-    //     const date = article.querySelector("h4")?.parentNode;
-    //
-    //     date.insertAdjacentElement("afterend", badge);
-    //
-    //
-    //
-    // }
-    let zzz=[{id:1, name:'first'}, {id:2, name:'second'}, {id:3, name:'third'}]
-
-    sendResponse(fields);
+    sendResponse(data);
 }
 
 
---
+++ viteVue3/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <el-button @click="go()">Go!</el-button>
+    9999999999999
+  </div>
+</template>
+
+<script setup>
+function go() {
+  console.log('GO!!!')
+}
+</script>
